3b3b209ab339e83976ab4c84a49cfa313ab8e696
Ashish Joshi <ashishjoshiacc@gmail.com>
2023-01-19 18:35:08 +0000

    modularized dim_customers into stg_customers and stg_orders

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -3,26 +3,18 @@
         materialized='table'
     )
 }}
-with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
 
-    from dbt-tutorial.jaffle_shop.customers
+with
+customers as (
+
+    select * from {{ ref ('stg_customers')}}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from dbt-tutorial.jaffle_shop.orders
+    select * from {{ ref ('stg_orders')}}
 
 ),
 
